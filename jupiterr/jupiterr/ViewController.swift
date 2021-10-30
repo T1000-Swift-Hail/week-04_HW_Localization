f98ffaa3655277ea3b231635fbcdf67b2e8508e2
Monafh <name@gmail.com>
2021-10-30 16:24:46 +0300

    OK

---
+++ jupiterr/jupiterr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jupiterr
+//
+//  Created by Monafh on 24/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
