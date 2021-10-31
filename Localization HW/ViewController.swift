863a58f471723dbe35f8ddfedc0335b78bf25f69
monaaleid <92249905+monaaleid@users.noreply.github.com>
2021-10-31 16:02:22 +0300

    udpate

---
+++ Localization HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Localization HW
+//
+//  Created by mona aleid on 24/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
